77249fb2ef857cfb971c2dcc5cdbae465de960d2
mtaylansahin <79852847+mtaylansahin@users.noreply.github.com>
2022-03-26 23:27:18 +0300

    Fix bug in fitness function

--- main.py
+++ main.py
@@ -82,10 +82,35 @@ class GeneticTSP:
 
     # Evaluate individual fitness
     def fitness(self, individual):
-        distance = 0
-        for start, end in enumerate(individual):
-            distance += self.get_edge(start, end)
-        return distance
+        """Caclulate total length of the given path, using the provided distance matrix.
+
+        Parameters
+        ----------
+        distance_matrix : NxN 2d array (numpy or list of lists)
+            Matrix of distance between nodes, only left triangle part is used.
+        path : sequence of integers
+            Path in the graph of nodes. Values are node indices, 0 .. N-1
+        Returns
+        -------
+        Length of the path. If path is empty or contains only 1 node, returns integer 0.
+        """
+        distance_matrix = self.edges
+        path = individual
+        ipath = iter(path)
+        try:
+            j = next(ipath)
+        except StopIteration:
+            #empty path
+            return 0
+
+        dist = 0    
+        for i in ipath:
+            if i >= j:
+                dist += distance_matrix[i][j]
+            else:
+                dist += distance_matrix[j][i]
+            j = i
+        return dist
 
     # Evaluate population fitness
     def population_fitness(self):
